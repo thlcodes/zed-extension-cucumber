c257be642a79846a50c5499bf9bf8518adb4e67b
Thomas Liebeskind <thomas.liebeskind@audi.de>
2024-04-24 22:28:35 +0200

    init

---
+++ languages/gherkin/highlights.scm
@@ -0,0 +1,25 @@
+[
+  (feature_keyword)
+  (background_keyword)
+  (scenario_keyword)
+  (scenario_outline_keyword)
+  (step_keywords)
+  (examples_keyword)
+] @keyword
+
+; (title) @string
+
+; (step_definition) @string
+(table_header_name) @attribute
+
+(comment) @comment
+
+(quoted_string) @string
+
+(number) @number
+
+(reference) @constant
+
+(tag) @label
+
+(table_cell) @constant
